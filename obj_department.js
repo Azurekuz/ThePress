2f6b7030265afef9aee359c1209e39814ba8632f
Eugene Kuznetsov <Azurekuz@gmail.com>
2019-05-08 12:04:30 -0400

    Notif Fix
    Removed the small notifications. That was all for a test.

--- obj_department.js
+++ obj_department.js
@@ -29,8 +29,10 @@ class obj_department{ //The department object
             this.phaserObject.setInteractive({useHandCursor: true}) 
                 .on('pointerdown', () => this.enterActiveState());
         }
-        this.needsNotify = true; //This needs a notification
-        this.game.notifications.addNotif(this, 'department'); //Add said notification
+        if(this.depName == 'ads'){
+            this.needsNotify = true; //This needs a notification
+            this.game.notifications.addNotif(this, 'department'); //Add said notification
+        }
     }
     
     accessDepartment(){
